dfee3facaa95f5348d9df20ec3e4ce78941847f4
Maxim Berezkin <maxim@docomoz.com>
2017-04-14 23:11:39 +0700

    Stable and works with any TLD

--- airbnb-search-beautifier.user.js
+++ airbnb-search-beautifier.user.js
@@ -4,22 +4,32 @@
 // @version      0.1
 // @description  Makes search results map bigger for a better user experience
 // @author       Maxim Berezkin
-// @match        https://www.airbnb.ru/*
+// @include      /^https?://(www\.)?airbnb\.(\w{2-3}\.)?\w+\/.*$/
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    var size = {
+        map: '70%',
+        panel: '30%'
+    };
+
     function airbnb_beautify() {
         var path = window.location.pathname.substring(1);
         if(/^s\//.test(path)) {
-            document.getElementsByClassName('search-results')[1].parentNode.parentNode.parentNode.style.cssText = 'width: 30% !important;';
-            document.getElementsByClassName('search-results-map')[0].parentNode.style.cssText = 'width: 70% !important;';
-            window.dispatchEvent(new Event('resize'));
+            var element = document.getElementsByClassName('search-results-map');
+            if(element.length) {
+                element[0].parentNode.style.cssText = 'width: ' + size.map + ' !important';
+                element[0].parentNode.previousSibling.style.cssText = 'width: ' + size.panel + ' !important';
 
-            document.getElementsByClassName('gm-bundled-control')[0].style.cssText = 'padding: 70px 0px 0px 10px !important;';
-            document.getElementsByClassName('gm-bundled-control')[0].childNodes[0].style.cssText = 'position: inherit !important;';
+                window.dispatchEvent(new Event('resize'));
+                element = document.getElementsByClassName('gm-bundled-control');
+                if(element.length) {
+                    element[0].childNodes[0].style.cssText = 'position: absolute; top: 65px; left: 0px';
+                }
+            }
         }
     }
 
